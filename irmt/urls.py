1e85c58e4b09664c6fc0fca925f5acf4804ed63d
nunchuckBoP <41088356+nunchuckBoP@users.noreply.github.com>
2019-01-18 16:21:37 -0600

    added code

--- irmt/urls.py
+++ irmt/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'^admin/', admin.site.urls),
+    path(r'^series/', include('app.urls'))
 ]
